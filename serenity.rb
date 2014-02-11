c9275f2aecc2793f18a3aa6b4287aff1c9676b72
Brian Jacobel <bjacobel@gmail.com>
2014-02-11 12:53:10 -0500

    Get pull requests from Events API

--- serenity.rb
+++ serenity.rb
@@ -2,10 +2,16 @@
 
 require 'octokit'
 
+# requires the env variables GITHUB_CLIENT_ID and GITHUB_CLIENT_SECRET to be set on the machine
 github = Octokit::Client.new :client_id => ENV['GITHUB_CLIENT_ID'], :client_secret => ENV['GITHUB_CLIENT_SECRET']
 
+# get all our repos - Serenity could be expanded to code outside 'nbites' later
+# serenity could even test itself. mind blown
 nbites_repos = github.organization_repositories('northern-bites')
 
-nbites_repos.each do |repo|
-  Octokit.repository_events(repo.full_name)
-end
+# for now, just find the nbites repo
+codebase = nbites_repos.select{|repo| repo.name == 'nbites'}[0]
+
+events = Octokit.repository_events(codebase.full_name)
+pull_requests = events.select{|event| event.type == "PullRequestEvent"} 
+pull_requests.each {|pr| puts pr.inspect + "\n\n"}
