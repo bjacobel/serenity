1e8a49948090de1c034952eeb16e20b1a53aa9c8
Brian Jacobel <bjacobel@gmail.com>
2014-03-13 17:15:05 -0400

    Create 'Build' model. Improved understanding of Delayed::Job

--- app/jobs/aws_job.rb
+++ app/jobs/aws_job.rb
@@ -1,22 +1,16 @@
 require 'date'
 
 class AwsJob
-  @ec2 = nil
-  @pkey = nil
 
-  def self.run
-    setup()
-    provision()
-    deploy()
-    test()
-    destroy()
-  end
+  def perform
 
-  def self.setup
+    # Do some setup - create an AWS object, get a keypair
+
     @ec2 = AWS::EC2.new(
       :access_key_id => Figaro.env.aws_key_id,
       :secret_access_key => Figaro.env.aws_key_secret
     )
+
     begin
       @pkey = @ec2.key_pairs.create("serenity").private_key
     rescue AWS::EC2::Errors::InvalidKeyPair::Duplicate
@@ -25,45 +19,64 @@ class AwsJob
       @ec2.key_pairs['serenity'].delete()
       @pkey = @ec2.key_pairs.create("serenity").private_key
     end
-  end
 
-  def self.provision
+
+    # request a spot instance
+
     request = @ec2.client.request_spot_instances({
-      :dry_run => true,
+      #:dry_run => true,
       :spot_price => "0.0075",  # youtu.be/M8KrUWeBt_s
       :instance_count => 1,
       :type => "one-time",
-      :valid_from => DateTime.now,
-      :valid_until => DateTime.now + 1/24, # wait up to an hour if neccesary
+      :valid_from => DateTime.now.strftime("%Y-%m-%dT%H:%M:%S"),
+      :valid_until => DateTime.now.advance(:hours => 1).strftime("%Y-%m-%dT%H:%M:%S"), # wait up to an hour if neccesary
       :launch_specification => {
         :key_name => "serenity",
-        :image_id => "ami-a18c8fc8",
+        :image_id => "ami-a18c8fc8",  # will need to change this later to run on every ami the serenity.yml specifies 
         :instance_type => "m1.small",
       },
     })
 
     # wait until the instance has been requisitioned and booted
+
     loop do
       description = @ec2.client.describe_spot_instance_requests({:spot_instance_request_ids => [request[:spot_instance_request_set][0][:spot_instance_request_id]]})
       if description[:spot_instance_request_set][0][:state] == 'active'
         return @ec2.instance.initialize(description[:spot_instance_request_set][0][:instance_id])
-      elsif description[:spot_instance_request_set][0][:state] == "closed"
-        return false
+      else
+        return nil
       end
     end
-  end
 
-  def self.deploy
 
-  end
 
-  def self.test
 
-  end
+    # break it down
 
-  def self.destroy
+    @instance.terminate
 
   end
+
+  def before(job)
+
+  end
+
+  def after(job)
+
+  end
+
+  def success(job)
+
+  end
+
+  def error(job, exception)
+
+  end
+
+  def failure(job)
+
+  end
+
 end
 
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140305040759) do
+ActiveRecord::Schema.define(version: 20140313211200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "builds", force: true do |t|
+    t.string   "status"
+    t.boolean  "passed"
+    t.text     "output"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0, null: false
